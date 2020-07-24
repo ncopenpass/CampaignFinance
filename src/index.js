ca3e5ac116f2aa917c6cd42f8885f9e9312e1721
Austin Wang <austin.wang0311@gmail.com>
2020-07-24 02:55:10 -0400

    Add react-uswds and node-sass, create header and footer components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './css/index.css';
+import './css/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
