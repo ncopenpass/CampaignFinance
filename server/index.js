ecfb3ff165c04fbba8e73003852448514b4046da
Schaffer Stewart <schaestewart@gmail.com>
2020-07-26 12:41:31 -0400

    Add prettier, husky, and lint-staged

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
-const { PORT: port = 3001 } = process.env;
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+const { PORT: port = 3001 } = process.env
 
-app.get('/status', (req, res) => res.send({'status': 'online'}))
+app.get('/status', (req, res) => res.send({ status: 'online' }))
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
