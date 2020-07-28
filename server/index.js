6f4808472aee385e079278f8b28aaa1187eb42f2
Schaffer Stewart <schaestewart@gmail.com>
2020-07-28 09:40:44 -0400

    Create contributor search endpoint, function

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { getClient } = require('./db')
+const { searchContributors } = require('./lib/search')
 const app = express()
 app.use(bodyParser.json())
 const { PORT: port = 3001 } = process.env
+const TRIGRAM_LIMIT = 0.6
 
+const api = express.Router()
+api.get('/search/contributors/:name', async (req, res) => {
+  try {
+    const { name } = req.params
+    const { offset = 0, limit = 50 } = req.query
+    const decodedName = decodeURIComponent(name)
+
+    const contributors = await searchContributors(
+      decodedName,
+      offset,
+      limit,
+      TRIGRAM_LIMIT
+    )
+    res.send(contributors)
+  } catch (error) {
+    console.error(error)
+    res.status(500)
+    res.send({
+      error: 'unable to process request',
+    })
+  }
+})
+
+app.use('/api', api)
 app.get('/status', (req, res) => res.send({ status: 'online' }))
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
---
+++ server/lib/search.js
@@ -0,0 +1,50 @@
+//@ts-check
+const { getClient } = require('../db')
+
+/**
+ * @typedef {Object} SearchResult
+ * @property {Array<any>} data
+ * @property {number} count
+ */
+
+/**
+ * @param {string} name
+ * @param {string|number} offset
+ * @param {string|number} limit
+ * @param {string|number} trigramLimit
+ * @returns {Promise<SearchResult>}
+ * @throws an error if the pg query or connection fails
+ */
+const searchContributors = async (
+  name,
+  offset = 0,
+  limit = 50,
+  trigramLimit = 0.6
+) => {
+  let client = null
+  try {
+    client = await getClient()
+    client.query('select set_limit($1)', [trigramLimit])
+    const results = await client.query(
+      `select *, similarity(name, $1) as sml 
+      from contributors where name % $1 
+      order by sml
+      limit $2 offset $3`,
+      [name, limit, offset]
+    )
+    return {
+      data: results.rows,
+      count: results.rowCount,
+    }
+  } catch (error) {
+    throw error
+  } finally {
+    if (client !== null) {
+      client.release()
+    }
+  }
+}
+
+module.exports = {
+  searchContributors,
+}
