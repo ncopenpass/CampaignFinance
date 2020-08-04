ef543afb51c7572ca34ac67c0bd55637c00f2ffc
Schaffer Stewart <SchaeStewart@users.noreply.github.com>
2020-08-04 22:30:47 +0000

    Add Candidate Search by Name (#53)
    * Add committee table
    
    * Implement candidate search by name
    
    * Update contributing
    
    * Fix response count. Add lookup by ncsbe_id
    
    * update api_doc
    
    * fix backtick

--- server/bin/etl.js
+++ server/bin/etl.js
@@ -6,16 +6,47 @@ const { getClient } = require('../db')
 const { copyFromCSV } = require('../db/copyFromCSV')
 
 ;(async () => {
-  const client = await getClient()
-  await copyFromCSV(
-    `${__dirname}/../tmp/contributors.csv`,
-    'contributors',
-    client
-  )
-  await copyFromCSV(
-    `${__dirname}/../tmp/contributions.csv`,
-    'contributions',
-    client
-  )
-  client.release()
+  let client = null
+  try {
+    client = await getClient()
+    await client.query(
+      'alter table committees drop column if exists candidate_full_name'
+    )
+    await client.query(
+      'alter table committees drop column if exists candidate_first_last_name'
+    )
+    await copyFromCSV(
+      `${__dirname}/../tmp/contributors.csv`,
+      'contributors',
+      client
+    )
+    await copyFromCSV(
+      `${__dirname}/../tmp/contributions.csv`,
+      'contributions',
+      client
+    )
+    await copyFromCSV(
+      `${__dirname}/../tmp/committees.csv`,
+      'committees',
+      client
+    )
+    await client.query(
+      'alter table committees add column candidate_full_name text'
+    )
+    await client.query(
+      'alter table committees add column candidate_first_last_name text'
+    )
+    await client.query(
+      `update committees set candidate_full_name = candidate_first_name || ' ' || candidate_middle_name || ' ' || candidate_last_name`
+    )
+    await client.query(
+      `update committees set candidate_first_last_name = candidate_first_name || ' ' || candidate_last_name`
+    )
+  } catch (error) {
+    console.error(error)
+  } finally {
+    if (client !== null) {
+      client.release()
+    }
+  }
 })()
