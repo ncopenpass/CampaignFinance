8c7b3958fffb2e3def4aa684c2be90c9db25d61d
Marc Celestini <marc.celestini@tanium.com>
2020-08-12 18:49:56 -0400

    add privacy policy page and integrate it in the app. this page is a placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import {
+  DONOR_ROUTE,
+  TABLE_ROUTE,
+  DATA_DICTIONARY_ROUTE,
+  DEFAULT_ROUTE,
+  PRIVACY_POLICY_ROUTE,
+} from '../constants'
+
 import Donor from './Donor'
 import Header from './Header'
 import Footer from './Footer'
 import Search from './Search'
 import Table from './Table'
 import DataDictionary from './DataDictionary'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import {
-  DONOR_ROUTE,
-  TABLE_ROUTE,
-  DATA_DICTIONARY_ROUTE,
-  DEFAULT_ROUTE,
-} from '../constants'
+import PrivacyPolicy from './PrivacyPolicy'
 
 export default function App() {
   return (
@@ -20,6 +24,9 @@ export default function App() {
         <Header></Header>
 
         <Switch>
+          <Route path={PRIVACY_POLICY_ROUTE}>
+            <PrivacyPolicy />
+          </Route>
           <Route path={DONOR_ROUTE}>
             <Donor />
           </Route>
