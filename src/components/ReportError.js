709c81d607374ec36bcaf8442cec8959367d71b6
alexisbarnes <alexis.barnes06@gmail.com>
2020-08-12 19:26:40 -0400

    Made report error btn with link to form.

---
+++ src/components/ReportError.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@trussworks/react-uswds'
+
+const ReportError = () => {
+  return (
+    <a href="https://forms.gle/EKWJiGnkogxWMgcY9" target="_blank">
+      <Button type="button" className="report-btn">
+        Report Error
+      </Button>
+    </a>
+  )
+}
+
+export default ReportError
