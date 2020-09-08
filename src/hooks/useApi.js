01e517f7ef0f91234f6afe402f1603ff3f415ade
Schaffer Stewart <SchaeStewart@users.noreply.github.com>
2020-09-08 19:00:49 -0400

    Fix search fetch (#125)

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -3,14 +3,14 @@ import { useCallback } from 'react'
 export const useApi = () => {
   const getDataAndCount = useCallback(async (url) => {
     try {
-      const { status, statusText, json } = await fetch(url)
+      const res = await fetch(url)
       // fetch will almost never throw an error 🤬
       // so we need to check the status code and throw the error ourselves
-      if (status >= 200 && status <= 299) {
-        const { data, count } = await json()
+      if (res.status >= 200 && res.status <= 299) {
+        const { data, count } = await res.json()
         return { data, count }
       } else {
-        throw Error(statusText)
+        throw Error(res.statusText)
       }
     } catch (e) {
       // catch network related errors here
