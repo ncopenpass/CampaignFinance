a053f17bfb1682dd7702bdce631a3f3d39244207
Schaffer Stewart <SchaeStewart@users.noreply.github.com>
2020-09-11 21:36:30 -0400

    Enforce SSL (#146)
    * enforce ssl in production
    
    * encforce https

--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
+//@ts-check
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+const enforceSSL = (req, res, next) => {
+  if (!req.secure) {
+    const host = req.headers['host'] ? req.headers['host'] : req.hostname
+    return res.redirect(301, 'https://' + host + req.url)
+  }
+  next()
+}
+
 const api = require('./api')
 
 const app = express()
+
+// Register this before all other handlers if we are in prod
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforceSSL)
+}
+
 app.use(bodyParser.json())
 app.use('/api', api)
 app.get('/status', (req, res) => res.send({ status: 'online' }))
