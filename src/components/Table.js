94d81ec0d5c8044ddad764abfbcfd3a578b7e527
Austin Wang <austin.wang0311@gmail.com>
2020-09-15 11:08:18 -0400

    Remove table filter/sort and add table size dropdown for MVP (#150)
    * Hide table filters
    
    * Add table size selection
    
    * Update quick search with fetch same function
    
    * Disable table sort functionality

--- src/components/Table.js
+++ src/components/Table.js
@@ -93,21 +93,7 @@ export default function Table({ columns, data }) {
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-                className={
-                  column.isSorted
-                    ? column.isSortedDesc
-                      ? 'sort-desc'
-                      : 'sort-asc'
-                    : ''
-                }
-              >
-                {column.render('Header')}
-                <div className="column-filter">
-                  {column.canFilter ? column.render('Filter') : null}
-                </div>
-              </th>
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
             ))}
           </tr>
         ))}
---
+++ src/hooks/useTablePagination.js
@@ -0,0 +1,29 @@
+import React, { useMemo } from 'react'
+
+export const useTablePagination = () => {
+  const tableLimits = useMemo(
+    () => [
+      {
+        label: 'Show 10',
+        value: '10',
+      },
+      {
+        label: 'Show 25',
+        value: '25',
+      },
+      {
+        label: 'Show 50',
+        value: '50',
+      },
+      {
+        label: 'Show 100',
+        value: '100',
+      },
+    ],
+    []
+  )
+
+  return {
+    tableLimits,
+  }
+}
