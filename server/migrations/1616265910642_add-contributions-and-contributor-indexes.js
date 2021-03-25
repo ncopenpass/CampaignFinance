cc76afa40c45d26dfc6bb799bc052a5bb3bcbb59
Schaffer Stewart <schaestewart@gmail.com>
2021-03-25 12:29:07 -0400

    Update migrations with new indexes

--- server/migrations/1616265910642_add-contributions-and-contributor-indexes.js
+++ server/migrations/1616265910642_add-contributions-and-contributor-indexes.js
@@ -8,10 +8,19 @@ exports.shorthands = undefined
  */
 exports.up = (pgm) => {
   pgm.sql(
-    `CREATE INDEX IF NOT EXISTS lower_sboe_id ON contributions ((lower(committee_sboe_id)));`
+    `CREATE INDEX IF NOT EXISTS contributions_lower_sboe_id ON contributions ((lower(committee_sboe_id)));`
   )
   pgm.sql(
     `CREATE INDEX IF NOT EXISTS contributor_name_trgm_idx ON contributors USING gist (name gist_trgm_ops);`
+  )
+  pgm.sql(
+    `CREATE INDEX IF NOT EXISTS committees_sboe_id on committees (sboe_id);`
+  )
+  pgm.sql(
+    `CREATE INDEX IF NOT EXISTS contributions_sboe_id on contributions (committee_sboe_id);`
+  )
+  pgm.sql(
+    `CREATE INDEX IF NOT EXISTS contributions_contributor_id on contributions (contributor_id);`
   )
 }
 
@@ -20,6 +29,9 @@ exports.up = (pgm) => {
  * @param {import('node-pg-migrate').MigrationBuilder} pgm
  */
 exports.down = (pgm) => {
-  pgm.sql(`DROP INDEX IF EXISTS lower_sboe_id;`)
+  pgm.sql(`DROP INDEX IF EXISTS contributions_lower_sboe_id;`)
   pgm.sql(`DROP INDEX IF EXISTS contributor_name_trgm_idx;`)
+  pgm.sql(`DROP INDEX IF EXISTS committees_sboe_id;`)
+  pgm.sql(`DROP INDEX IF EXISTS contributions_sboe_id;`)
+  pgm.sql(`DROP INDEX IF EXISTS contributions_contributor_id;`)
 }
