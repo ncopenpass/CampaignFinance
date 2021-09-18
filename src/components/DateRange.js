05d4e7be21fd819b59213f31f2f69e5e088c1bfa
alexisbarnes <alexis.barnes06@gmail.com>
2021-09-17 20:37:21 -0400

    set up date picker

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -1,50 +1,56 @@
-import React from 'react'
-import { GridContainer, Grid } from '@trussworks/react-uswds'
+import React, { useEffect, useState } from 'react'
+import { Grid } from '@trussworks/react-uswds'
 import { Form, DateRangePicker, Dropdown } from '@trussworks/react-uswds'
 
 import '../css/date.scss'
 
-const DateRange = () => {
+const DateRange = ({ allYears = ['2020', '2019', '2018'] }) => {
+  const [year, setYear] = useState('')
+
+  const date = new Date()
+  const getYear = date.getFullYear()
+
+  let handleYear = (e) => {
+    setYear(e.target.value)
+  }
+
   return (
-    <GridContainer>
+    <>
       <Grid row>
-        <Grid col>
-          <h2>Select a year or pick a date range</h2>
-        </Grid>
+        <Dropdown
+          id="input-dropdown"
+          name="input-dropdown"
+          onChange={handleYear}
+        >
+          <option value={year}>- All Years -</option>
+          {allYears.map((year) => (
+            <option value={year}>{year}</option>
+          ))}
+        </Dropdown>
       </Grid>
       <Grid row>
-        <Grid col>
-          <Dropdown id="input-dropdown" name="input-dropdown">
-            <option value="value1">- Select Year -</option>
-            <option value="value2">Option A</option>
-            <option value="value3">Option B</option>
-            <option value="value4">Option c</option>
-          </Dropdown>
-        </Grid>
-        <Grid col>
-          <Form onSubmit={function noRefCheck() {}} className="date-range-form">
-            <DateRangePicker
-              startDateHint="mm/dd/yyyy"
-              startDateLabel="Event start date"
-              startDatePickerProps={{
-                defaultValue: '2021-01-20',
-                disabled: false,
-                id: 'event-date-start',
-                name: 'event-date-start',
-              }}
-              endDateHint="mm/dd/yyyy"
-              endDateLabel="End date"
-              endDatePickerProps={{
-                defaultValue: '2021-01-25',
-                disabled: false,
-                id: 'event-date-end',
-                name: 'event-date-end',
-              }}
-            />
-          </Form>
-        </Grid>
+        <Form onSubmit={function noRefCheck() {}} className="date-range-form">
+          <DateRangePicker
+            startDateHint="mm/dd/yyyy"
+            startDateLabel="Event start date"
+            startDatePickerProps={{
+              defaultValue: !year ? getYear + '-01-01' : year + '-01-01',
+              disabled: false,
+              id: 'event-date-start',
+              name: 'event-date-start',
+            }}
+            endDateHint="mm/dd/yyyy"
+            endDateLabel="End date"
+            endDatePickerProps={{
+              defaultValue: '2021-01-25',
+              disabled: false,
+              id: 'event-date-end',
+              name: 'event-date-end',
+            }}
+          />
+        </Form>
       </Grid>
-    </GridContainer>
+    </>
   )
 }
 
