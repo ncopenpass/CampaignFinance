1cacf1f69958326e53f47bd4c9f52eb0f72ba370
rajeshg <gollapudi.rajesh@gmail.com>
2021-10-25 14:11:28 -0400

    Fix 271: Remove commented out code

--- scripts/extractor.js
+++ scripts/extractor.js
@@ -7,9 +7,7 @@ let path = require("path");
 // reading archives
 let zip = new AdmZip("data.zip");
 zip.extractAllTo("", true);
-// fs.createReadStream('data.zip').pipe(unzip.Extract({ path: '.' }));
 const mainEntry = zip.getEntries()[0].entryName;
-// copySync(path.join(tempDir, mainEntry), srcPath, {overwrite: true});
 fs.readdirSync(mainEntry).forEach(file => {
     console.log('Copying file: ' + file);
     fs.renameSync(path.join(mainEntry, file), file);
