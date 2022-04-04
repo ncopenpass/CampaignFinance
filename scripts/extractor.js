0869844a69e832de14b306278edd915dd80e28b0
alexisbarnes <alexis.barnes06@gmail.com>
2022-04-04 19:55:37 -0400

    fix: add adm-zip and add nvmrc

--- scripts/extractor.js
+++ scripts/extractor.js
@@ -1,15 +1,19 @@
-'use strict';
+'use strict'
 
 const fs = require('fs')
-let AdmZip = require("adm-zip");
-let path = require("path");
+let AdmZip = require('adm-zip')
+let path = require('path')
 
 // reading archives
-let zip = new AdmZip("data.zip");
-zip.extractAllTo("", true);
-const mainEntry = zip.getEntries()[0].entryName;
-fs.readdirSync(mainEntry).forEach(file => {
-    console.log('Copying file: ' + file);
-    fs.renameSync(path.join(mainEntry, file), file);
-  });
-fs.rmdirSync(mainEntry);
+try {
+  let zip = new AdmZip('data.zip')
+  zip.extractAllTo('', true)
+  const mainEntry = zip.getEntries()[0].entryName
+  fs.readdirSync(mainEntry).forEach((file) => {
+    console.log('Copying file: ' + file)
+    fs.renameSync(path.join(mainEntry, file), file)
+  })
+  fs.rmdirSync(mainEntry)
+} catch (err) {
+  console.log(err)
+}
